1930be7d82464f3e9efb69db5ca49b55b176625c
Deivis <deiviswingert@hotmail.com>
2017-11-08 16:36:28 -0200

    Api access: add transaction logic to the api index

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,39 @@
+import pagarme from 'pagarme'
 
+import CreditCardTransaction from './creditCardTransaction'
+
+let client
+
+export function connect() {
+  if (!client) {
+    return pagarme.client
+      .connect({
+        encryption_key: process.env.API_TEST_ENCRIPT_KEY,
+      })
+      .then((clientResponse) => {
+        client = clientResponse
+        return client
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+  return Promise.resolve(client)
+}
+
+const formatCardData = ({
+  cardNumber,
+  cardHolderName,
+  cardExpirationDate,
+  cardCvv,
+}) =>
+  ({
+    card_number: cardNumber,
+    card_holder_name: cardHolderName,
+    card_expiration_date: cardExpirationDate,
+    card_cvv: `${cardCvv}`,
+  })
+
+export default {
+  creditCardTransaction: CreditCardTransaction(connect, formatCardData),
+}
