831a163ef1f0be8f2ebd33673fb9b69437ab48f4
Deivis <deiviswingert@hotmail.com>
2017-11-10 10:18:41 -0200

    General: add mock for credit cards

---
+++ src/api/mock.js
@@ -0,0 +1,18 @@
+export const invalidCard = {
+  cardNumber: '5256690157847094',
+  cardHolderName: 'xuxu',
+  cardExpirationDate: '0918',
+  cardCvv: '678',
+}
+
+export const validCard = {
+  cardNumber: '347722449382044',
+  cardHolderName: 'xuxu',
+  cardExpirationDate: '0818',
+  cardCvv: '882',
+}
+
+export default {
+  invalidCard,
+  validCard,
+}
