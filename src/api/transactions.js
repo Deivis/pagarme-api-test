5d0dccdf692321976d05d2e4df92cdc1cecc416d
Deivis <deiviswingert@hotmail.com>
2017-11-10 10:17:47 -0200

    Transactions: add a filter in the query which get "all" transactions  to bring only transactions from 11-06-2017 until the current day

--- src/api/transactions.js
+++ src/api/transactions.js
@@ -3,7 +3,10 @@ let connect
 
 async function getAll() {
   client = await connect()
-  return client.transactions.all()
+  return client.transactions.all({
+    date_created: `>=${new Date('2017-11-06').getTime()}`,
+    count: 0,
+  })
 }
 
 async function create(transactionParams) {
@@ -11,10 +14,11 @@ async function create(transactionParams) {
   return client.transactions.create(transactionParams)
 }
 
-async function capture(transactionId) {
+async function capture(transactionId, amount) {
   client = await connect()
   return client.transactions.capture({
     id: transactionId,
+    amount,
   })
 }
 
