4eacd15c0e70a5e4982b86734f267f322bce8a4b
Deivis <deiviswingert@hotmail.com>
2017-11-10 10:43:10 -0200

    Creditcard transaction: add postback url in the api call

--- src/api/creditCardTransaction.js
+++ src/api/creditCardTransaction.js
@@ -7,21 +7,25 @@ let formatCardData
 
 const mutiplyBy100 = value => value * 100
 
-const formatTransactionData = (cardData, amount, installments) => {
+const formatTransactionData = (cardData, amount, installments, postbackUrl) => {
   if (cardData && amount) {
     const amountInCents = pipe(parseFloat, mutiplyBy100)(amount)
     // eslint-disable-next-line
     if (installments && !isNaN(installments)) {
-      return merge(formatCardData(cardData), { amount: amountInCents, installments })
+      return merge(formatCardData(cardData), {
+        amount: amountInCents,
+        installments,
+        postback_url: postbackUrl,
+      })
     }
-    return merge(formatCardData(cardData), { amount: amountInCents })
+    return merge(formatCardData(cardData), { amount: amountInCents, postback_url: postbackUrl })
   }
   throw new Error('CardData or ammount are missing')
 }
 
-async function creditCardTransaction(cardData, amount, installments) {
+async function creditCardTransaction(cardData, amount, installments, postbackUrl) {
   const { create } = Transactions(connect)
-  return create(formatTransactionData(cardData, amount, installments))
+  return create(formatTransactionData(cardData, amount, installments, postbackUrl))
 }
 
 export default (mainConnect, mainFormatCardData) => {
