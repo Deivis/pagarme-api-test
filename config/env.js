6a6075a6ca9db89c0912f8075dd2b50468051c9e
Deivis <deiviswingert@hotmail.com>
2017-11-10 10:10:14 -0200

    General: change the expression which get the .env variables

--- config/env.js
+++ config/env.js
@@ -57,25 +57,24 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 const REACT_APP = /(^REACT_APP_)|(_TEST_)/gi
 
 function getClientEnvironment(publicUrl) {
+  const baseEnv = {
+    // Useful for determining whether we’re running in production mode.
+    // Most importantly, it switches React into the correct mode.
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    // Useful for resolving the correct path to static assets in `public`.
+    // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
+    // This should only be used as an escape hatch. Normally you would put
+    // images into the `src` and `import` them in code to get their paths.
+    PUBLIC_URL: publicUrl,
+  }
+
   const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
-    .reduce(
-      (env, key) => {
-        const envAux = env
-        envAux[key] = process.env[key]
-        return envAux
-      },
-      {
-      // Useful for determining whether we’re running in production mode.
-      // Most importantly, it switches React into the correct mode.
-        NODE_ENV: process.env.NODE_ENV || 'development',
-        // Useful for resolving the correct path to static assets in `public`.
-        // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
-        // This should only be used as an escape hatch. Normally you would put
-        // images into the `src` and `import` them in code to get their paths.
-        PUBLIC_URL: publicUrl,
-      },
-    )
+    .filter(key => key.match(REACT_APP))
+    .reduce((env, key) => {
+      const envAux = env
+      envAux[key] = process.env[key]
+      return envAux
+    }, baseEnv)
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
     'process.env': Object.keys(raw).reduce((env, key) => {
